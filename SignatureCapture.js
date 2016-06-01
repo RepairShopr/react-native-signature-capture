3d103fb94c03eee593c8c9856de92f78bd593627
Adrian Bak <adrianbak84@gmail.com>
2016-06-01 14:38:27 +0200

    added back the DeviceEventEmitter so it actually calls the save prop

--- SignatureCapture.js
+++ SignatureCapture.js
@@ -9,7 +9,8 @@ var {
 var {
     requireNativeComponent,
     View,
-    UIManager
+    UIManager,
+    DeviceEventEmitter
 } = ReactNative;
 
 class SignatureCapture extends React.Component {
@@ -20,29 +21,41 @@ class SignatureCapture extends React.Component {
     }
 
     onChange(event) {
-        console.log("Signature  ON Change Event");
-        
-        
-       if(event.nativeEvent.pathName){
-           
-           if (!this.props.onSaveEvent) {
-            return;
+
+        if(event.nativeEvent.pathName){
+
+            if (!this.props.onSaveEvent) {
+                return;
+            }
+            this.props.onSaveEvent({
+                pathName: event.nativeEvent.pathName,
+                encoded: event.nativeEvent.encoded,
+            });
         }
-        this.props.onSaveEvent({
-            pathName: event.nativeEvent.pathName,
-            encoded: event.nativeEvent.encoded,
-        });
-       }
-       
-       if(event.nativeEvent.dragged){
-           if (!this.props.onDragEvent) {
-            return;
+
+        if(event.nativeEvent.dragged){
+            if (!this.props.onDragEvent) {
+                return;
+            }
+            this.props.onDragEvent({
+                dragged: event.nativeEvent.dragged
+            });
         }
-        this.props.onDragEvent({
-            dragged: event.nativeEvent.dragged
-        });
-       }
-       }
+    }
+
+    componentDidMount() {
+        this.subscription = DeviceEventEmitter.addListener(
+            'onSaveEvent',
+            this.props.onSaveEvent
+        );
+    }
+
+    componentWillUnmount() {
+        if (this.subscription) {
+            this.subscription.remove()
+            this.subscription = null;
+        }
+    }
 
     render() {
         return (
