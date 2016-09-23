e4f7c7852ec911289185de25ebde84a00d4dc16f
Alexander Soto <alexsotocx@gmail.com>
2016-09-23 15:55:25 -0500

    Working android

--- SignatureCapture.js
+++ SignatureCapture.js
@@ -1,88 +1,87 @@
-var React, { PropTypes, Component } import 'react';
-var ReactNative, {
+import React, { Component, PropTypes } from 'react';
+import ReactNative, {
   requireNativeComponent,
   View,
   UIManager,
   DeviceEventEmitter
-} import 'react-native';
-
+} from 'react-native';
 
 class SignatureCapture extends Component {
 
-    constructor() {
-        super();
-        this.onChange = this.onChange.bind(this);
+  constructor() {
+    super();
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
+
+    if(event.nativeEvent.pathName){
+      if (this.props.onSaveEvent) {
+        this.props.onSaveEvent({
+          pathName: event.nativeEvent.pathName,
+          encoded: event.nativeEvent.encoded,
+        });
+      }
     }
 
-    onChange(event) {
-
-        if(event.nativeEvent.pathName){
-            if (this.props.onSaveEvent) {
-              this.props.onSaveEvent({
-                  pathName: event.nativeEvent.pathName,
-                  encoded: event.nativeEvent.encoded,
-              });
-            }
-        }
-
-        if(event.nativeEvent.dragged){
-            if (this.props.onDragEvent) {
-              this.props.onDragEvent({
-                  dragged: event.nativeEvent.dragged
-              });
-            }
-        }
+    if(event.nativeEvent.dragged){
+      if (this.props.onDragEvent) {
+        this.props.onDragEvent({
+          dragged: event.nativeEvent.dragged
+        });
+      }
     }
+  }
 
-    componentDidMount() {
-        this.subscription = DeviceEventEmitter.addListener(
-            'onSaveEvent',
-            this.props.onSaveEvent
-        );
-    }
+  componentDidMount() {
+    this.subscription = DeviceEventEmitter.addListener(
+      'onSaveEvent',
+      this.props.onSaveEvent
+    );
+  }
 
-    componentWillUnmount() {
-        if (this.subscription) {
-            this.subscription.remove()
-            this.subscription = null;
-        }
+  componentWillUnmount() {
+    if (this.subscription) {
+      this.subscription.remove();
+      this.subscription = null;
     }
+  }
 
-    render() {
-        return (
-            <RSSignatureView {...this.props} onChange={this.onChange} />
-        );
-    }
+  render() {
+    return (
+      <RSSignatureView {...this.props} onChange={this.onChange} />
+    );
+  }
 
-    saveImage() {
-        UIManager.dispatchViewManagerCommand(
-            ReactNative.findNodeHandle(this),
-            UIManager.RSSignatureView.Commands.saveImage,
-            [],
-        );
-    }
+  saveImage() {
+    UIManager.dispatchViewManagerCommand(
+      ReactNative.findNodeHandle(this),
+      UIManager.RSSignatureView.Commands.saveImage,
+      []
+    );
+  }
 
-    resetImage() {
-        UIManager.dispatchViewManagerCommand(
-            ReactNative.findNodeHandle(this),
-            UIManager.RSSignatureView.Commands.resetImage,
-            [],
-        );
-    }
+  resetImage() {
+    UIManager.dispatchViewManagerCommand(
+      ReactNative.findNodeHandle(this),
+      UIManager.RSSignatureView.Commands.resetImage,
+      []
+    );
+  }
 }
 
 SignatureCapture.propTypes = {
   ...View.propTypes,
-    rotateClockwise: PropTypes.bool,
-    square: PropTypes.bool,
-    saveImageFileInExtStorage: PropTypes.bool,
-    viewMode: PropTypes.string,
-    showNativeButtons: PropTypes.bool,
-    maxSize:PropTypes.number
+  rotateClockwise: PropTypes.bool,
+  square: PropTypes.bool,
+  saveImageFileInExtStorage: PropTypes.bool,
+  viewMode: PropTypes.string,
+  showNativeButtons: PropTypes.bool,
+  maxSize:PropTypes.number
 };
 
 var RSSignatureView = requireNativeComponent('RSSignatureView', SignatureCapture, {
-    nativeOnly: { onChange: true }
+  nativeOnly: { onChange: true }
 });
 
 export default SignatureCapture;
