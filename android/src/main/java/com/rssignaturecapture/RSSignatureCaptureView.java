a6724b997444ca774b7f01e6486124d921caa041
Antonio Grass <antoniograssb@gmail.com>
2017-03-03 10:21:59 -0300

    change android stroke

--- android/src/main/java/com/rssignaturecapture/RSSignatureCaptureView.java
+++ android/src/main/java/com/rssignaturecapture/RSSignatureCaptureView.java
@@ -68,8 +68,8 @@ public class RSSignatureCaptureView extends View {
 		mPaint.setStrokeCap(Paint.Cap.ROUND);
 		mPaint.setStrokeJoin(Paint.Join.ROUND);
 
-		mMinWidth = convertDpToPx(8);
-		mMaxWidth = convertDpToPx(16);
+		mMinWidth = convertDpToPx(4);
+		mMaxWidth = convertDpToPx(8);
 		mVelocityFilterWeight = 0.4f;
 		mPaint.setColor(Color.BLACK);
 
