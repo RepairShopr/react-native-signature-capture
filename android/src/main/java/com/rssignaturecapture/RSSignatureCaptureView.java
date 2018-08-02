75938d04928cab93c8d8b375a12bbb9a11d8d9e6
vlad.gus <v.gus@softteco.com>
2018-08-02 16:21:03 +0300

    Update RSSignatureCaptureView.java
    Changed signature background to transparent.

--- android/src/main/java/com/rssignaturecapture/RSSignatureCaptureView.java
+++ android/src/main/java/com/rssignaturecapture/RSSignatureCaptureView.java
@@ -92,18 +92,21 @@ public class RSSignatureCaptureView extends View {
 	* @return
 	*/
 	public Bitmap getSignature() {
-
-		Bitmap signatureBitmap = null;
-
-		// set the signature bitmap
-		if (signatureBitmap == null) {
-			signatureBitmap = Bitmap.createBitmap(this.getWidth(), this.getHeight(), Bitmap.Config.RGB_565);
-		}
+		// Initialize a Bitmap to contain the signature
+		Bitmap signatureBitmap = Bitmap.createBitmap(this.getWidth(), this.getHeight(), Bitmap.Config.ARGB_8888);
 
 		// important for saving signature
 		final Canvas canvas = new Canvas(signatureBitmap);
 		this.draw(canvas);
-
+		// The signatureBitmap now contains the signature with a white background,
+		// so we will change it to a transparent background.
+		for (int x = 0; x < signatureBitmap.getWidth(); x++) {
+			for (int y = 0; y < signatureBitmap.getHeight(); y++) {
+				if (Color.WHITE == signatureBitmap.getPixel(x, y)) {
+					signatureBitmap.setPixel(x, y, Color.TRANSPARENT);
+				}
+			}
+		}
 		return signatureBitmap;
 	}
 
