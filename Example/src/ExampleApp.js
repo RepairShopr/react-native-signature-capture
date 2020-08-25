261cf633468c0b11bb80e00691df0400e6853cdc
Frieder Bluemle <frieder.bluemle@gmail.com>
2020-08-25 01:33:26 -0700

    Update example react-native to 0.63.2

--- Example/src/ExampleApp.js
+++ Example/src/ExampleApp.js
@@ -1,67 +1,70 @@
-import React, { Component } from 'react';
-
+import React, {useState, useRef} from 'react';
 import {
-  View, StyleSheet, Text, Image, TouchableOpacity
+  View,
+  StyleSheet,
+  Button,
+  Text,
+  Image,
+  TouchableOpacity,
 } from 'react-native';
-
 import SignatureView from './SignatureView';
 
-const flexCenter = {
-  flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center',
+const ExampleApp: () => React$Node = () => {
+  const [data, setData] = useState(null);
+  const signatureView = useRef(null);
+
+  const onSave = function (result) {
+    setData(`data:image/png;base64,${result.encoded}`);
+    signatureView.current.show(false);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={() => {
+          signatureView.current.show(true);
+        }}>
+        <View>
+          <Text style={styles.titleText}>
+            {data ? 'Your signature:' : 'Click to sign'}
+          </Text>
+          {data && (
+            <View style={styles.imageContainer}>
+              <Image style={styles.previewImage} source={{uri: data}} />
+              <Button title="Clear" onPress={() => setData(null)} />
+            </View>
+          )}
+        </View>
+      </TouchableOpacity>
+      <SignatureView
+        ref={signatureView}
+        rotateClockwise={true}
+        onSave={onSave}
+      />
+    </View>
+  );
 };
 
-class ExampleApp extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      data: null
-    };
-  }
-
-  render() {
-    const {data} = this.state;
-    return (
-      <View style={flexCenter}>
-        <TouchableOpacity onPress={this._showSignatureView.bind(this)}>
-          <View style={[flexCenter, {padding: 10}]}>
-            <Text style={{fontSize: 18, fontWeight: 'bold'}}>
-              {data ? 'This is a your signature.' : 'Click here.'}
-            </Text>
-            <View style={{paddingBottom: 10}} />
-            {data &&
-              <View style={{backgroundColor: 'white'}}>
-                <Image
-                  resizeMode={'contain'}
-                  style={{width: 300, height: 300}}
-                  source={{uri: data}}
-                />
-              </View>
-            }
-          </View>
-        </TouchableOpacity>
-        <SignatureView
-          ref={r => this._signatureView = r}
-          rotateClockwise={!!true}
-          onSave={this._onSave.bind(this)}
-        />
-      </View>
-    );
-  }
-
-  _showSignatureView() {
-    this._signatureView.show(true);
-  }
-
-  _onSave(result) {
-    const base64String = `data:image/png;base64,${result.encoded}`;
-    this.setState({data: base64String});
-
-    this._signatureView.show(false);
-  }
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#F8F8F8',
+    alignItems: 'center',
+  },
+  titleText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  imageContainer: {
+    marginTop: 10,
+    backgroundColor: 'white',
+  },
+  previewImage: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
+  },
+});
 
 export default ExampleApp;
